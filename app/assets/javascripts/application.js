d0c0c0d7a8ac15c08d9afaba87c2afb542302018
anjain <ror.upwork@gmail.com>
2016-12-06 17:37:57 +0530

    AdminLte theme added ecept for home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require ./admin/app
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def body_class(body_wrapper = '')
+    if body_wrapper.blank?
+      'skin-blue sidebar-mini wysihtml5-supported'
+    else
+      body_wrapper
+    end
+  end
 end
